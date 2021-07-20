5562b6a9dffa8e81e7a365bd81ec2515f9d0b800
parakramlama <tmngparakram@gmail.com>
2021-07-20 09:59:03 +0545

    verify admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,7 @@ module.exports.verifyUser = function(req, res, next){
         console.log(token)
         const data = jwt.verify(token, 'anysecretkey');
         console.log(data)
-        
+
         //in this data id is available
         User.findOne({_id : data.userID})
         .then(function(userData){
@@ -26,4 +26,23 @@ module.exports.verifyUser = function(req, res, next){
         res.status(401).json({error : e})
         
     }    
+}
+
+
+//1. get token from client
+//2. verify the token
+//3. false- return with error msg
+//4. true- get id from token
+//5. fetch all the info from db using the id
+
+
+module.exports.verifyAdmin = function(req,res,next){
+    console.log(req.user)
+    if(!req.user){
+        return res.status(401).json({message : "Unauthorized(Not logged in)"})
+    }
+    else if(req.user.user_role != "Admin"){
+        return res.status(401).json({message : "Unauthorized(Not Admin)"})
+    }
+    next()
 }
