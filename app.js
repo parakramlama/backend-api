f1c1ff5a71b0f2a18c4af0c931ef60844d1bd38d
parakramlama <tmngparakram@gmail.com>
2021-07-20 09:46:18 +0545

    app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose');
+const express=require('express');
+const bodyParser=require('body-parser')
+const cors = require('cors');
+
+const db=require('./database/db');
+const user_route=require('./routes/user_route');
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/public', express.static(__dirname + '/public'));
+
+
+app.use(user_route);
+
+app.listen(90);
