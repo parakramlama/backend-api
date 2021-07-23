6bc157946d44da79e23a5bf861f4a2ee8735c3ca
parakramlama <tmngparakram@gmail.com>
2021-07-23 11:12:38 +0545

    insert cart

--- routes/cart_route.js
+++ routes/cart_route.js
@@ -41,4 +41,42 @@ router.delete('/cart/:id', auth.verifyUser, function(req, res){
         })
 })
 
+router.post('/cart/insert',auth.verifyUser, function(req, res){
+    var data = {
+        product : req.body.productId,
+        user:  req.user._id,
+    }
+
+    Cart.findOne({user : req.user._id , product : req.body.productId}, function(err, cart){
+        if(cart){
+            var currentquantity = cart.quantity+1;
+            Cart.findOneAndUpdate({_id : cart._id}, {$set : {quantity : currentquantity}}).then(function(cart){
+                return res.status(200).json({
+                    success : true,
+                    message : "Cart Successfully Added",
+                    cart : cart
+                })
+            }).catch(err => {
+                return res.status(500).json({
+                    succeess : false,
+                    messsage : err.message
+                })
+            })
+        }else{
+            Cart.create(data).then(function(cart){
+                return res.status(200).json({
+                    success : true,
+                    message : "Cart Successfully Added",
+                    cart : cart
+                })
+            }).catch(err => {
+                return res.status(500).json({
+                    succeess : false,
+                    messsage : err.message
+                })
+            })
+        }
+    })
+})
+
 module.exports=router;
