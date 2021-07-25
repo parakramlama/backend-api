a1fa53f852f72a7b94950c82d90867fec1d639cd
parakramlama <tmngparakram@gmail.com>
2021-07-25 11:32:35 +0545

    chnanges in app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 const db=require('./database/db');
 const user_route=require('./routes/user_route');
 const product_route=require('./routes/product_route');
-const cart_route=require('./routes/cart_route')
+const cart_route=require('./routes/cart_route');
+const article_route=require('./routes/article_route');
+const contact_route=require('./routes/contact_route');
 
 const app=express();
 app.use(cors());
@@ -19,5 +21,7 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use(user_route);
 app.use(product_route);
 app.use(cart_route);
+app.use(article_route);
+app.use(contact_route);
 
 app.listen(90);
