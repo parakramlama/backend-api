d3f7bfa0a5a8e605cd3e10bfda9c285834dd813a
parakramlama <tmngparakram@gmail.com>
2021-07-25 11:23:29 +0545

    article model added

---
+++ models/article_model.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+
+const Article=mongoose.model('Article',{
+    article_title:{
+        type : String,
+        required : true
+    },
+    article_body:{
+        type:String,
+        required : true
+    },
+})
+
+module.exports=Article;
